98b49964fdc8611acf64ce7697fd84bd869fecc7
jkapnick <jkapnick24@gmail.com>
2022-02-06 17:58:58 -0600

    Create Challenge-3.py

---
+++ Challenge-3.py
@@ -0,0 +1,6 @@
+# Implement a function which convert the given boolean value into its string representation.
+# Note: Only valid inputs will be given.
+def convert_boolean(b):
+    new_string = str(b)
+
+    return new_string
